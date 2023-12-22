c3b4d50b85f323300b46703bd597a1de8ab95bc4
Tanishka khamesara <tanishkajain24941@gmail.com>
2023-12-22 23:51:28 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
+import Body from "./Components/Body";
+import Text from "./Components/Text";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    // <div className="App">
+    <div>
+      <Header />
+
+      <div className="flexprop" style={{
+        display: "flex",
+      }}>
+        <Navbar name="HOME" />
+        <Navbar name="ABOUT"/>
+        <Navbar name="SERVICES"/>
+        <Navbar name="FAQ"/>
+        <Navbar name="CONTACT"/>
+      </div>
+      <div class="image">
+      <img src="https://www.thelifeacademygroup.com/wp-content/uploads/2021/10/testing-image2.jpg"></img>
+      </div>
+      <div class="main">
+        <Body />
+      </div>
+      <div class="text">
+        <Text />
+      </div>
+      <br /> <br />
+      <div class="footer">
+        <Footer />
+      </div>
+      <div class="footWidth">
+        <p>Powered by Website.com</p>
+      </div>
+    </div>
+   
+  );
+}
+
+export default App;
